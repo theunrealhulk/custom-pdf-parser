905ecc7803fdb10fd4c65c60a95f84d344a41f8e
JC <chafiaay@protonmail.com>
2020-05-02 03:57:17 +0000

    ReadPdf entire text

---
+++ DDPdftxt.cs
@@ -0,0 +1,30 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pdfparser
+{
+    public static class DDPdftxt
+    {   
+        /// <summary>
+        /// Get Dictionary (pageNumber,Page text as list of lines)
+        /// </summary>
+        /// <param name="path"> path to the pdf file</param>
+        /// <returns></returns>
+        public static Dictionary<int, List<string>> ReadDDPdf(string path)
+        {
+            PdfReader reader = new PdfReader(path);
+            Dictionary<int, List<string>> output = new Dictionary<int, List<string>>();
+            //getting text from pdg page by page
+            for (int i = 0; i < reader.NumberOfPages; i++)
+            {
+                output.Add(i + 1, (PdfTextExtractor.GetTextFromPage(reader, i + 1).Replace("\n", "|").Split('|').ToList()));
+            }
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+namespace Pdfparser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo("/media/usb/SSD/work/Cshap/pdf-simples");
+                int i=0;
+                var files = directoryInfo.GetFiles();
+                while (i < files.Count())
+                {
+                    var f = files[i];
+                    if (f.Extension==".pdf")
+                    {
+                        Dictionary<int, List<string>> output = new Dictionary<int, List<string>>();
+                        output = DDPdftxt.ReadDDPdf(f.FullName);
+                         
+         
+                    }
+                    i++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
